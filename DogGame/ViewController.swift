9f7d02f54f7c916af179cb11c18c7a1417f06f7b
ItalusR <italus.prado@hotmail.com>
2016-11-18 09:30:02 -0300

    projeto doggame com statusbar

--- DogGame/ViewController.swift
+++ DogGame/ViewController.swift
@@ -10,14 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var raivaBar: UIView!
+    @IBOutlet weak var fomeBar: UIView!
+    @IBOutlet weak var saudeBar: UIView!
+    
+    @IBOutlet weak var raivaLabel: UILabel!
+    @IBOutlet weak var fomeLabel: UILabel!
+    @IBOutlet weak var saudeLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        generateRandomStatus()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func generateRandomStatus(){
+        
+        let barra1: Float = 1+Float(arc4random()%240)
+        let barra2: Float = 1+Float(arc4random()%240)
+        let barra3: Float = 1+Float(arc4random()%240)
+        
+        self.raivaLabel.text = "Raiva: \(barra1)"
+        self.fomeLabel.text = "Fome: \(barra2)"
+        self.saudeLabel.text = "Saúde: \(barra3)"
+        
+        self.raivaBar.frame.size.width = CGFloat(barra1)
+        self.fomeBar.frame.size.width = CGFloat(barra2)
+        self.saudeBar.frame.size.width = CGFloat(barra3)
+        
+        self.raivaBar.backgroundColor = UIColor(colorLiteralRed: (255-barra1)/255, green: barra1/255, blue: 0, alpha: 1)
+        self.fomeBar.backgroundColor = UIColor(colorLiteralRed: (255-barra2)/255, green: barra2/255, blue: 0, alpha: 1)
+        self.saudeBar.backgroundColor = UIColor(colorLiteralRed: (255-barra3)/255, green: barra3/255, blue: 0, alpha: 1)
+    
     }
 
 
