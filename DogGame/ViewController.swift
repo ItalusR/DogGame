663628580a58c5e410c90c49bed9d56b93ab0ced
ItalusR <italus.prado@hotmail.com>
2016-11-18 08:57:29 -0300

    Initial Commit

---
+++ DogGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DogGame
+//
+//  Created by Italus Rodrigues do Prado on 18/11/16.
+//  Copyright © 2016 GAMEPiD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
